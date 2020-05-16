5779f2b1e2d0b2626c9aa1a586b91dc5b7e97fdd
Jonathan <jonathan.bereyziat@gmail.com>
2020-05-16 20:43:19 +0200

    Formating gameboy.c

--- done/alu.c
+++ done/alu.c
@@ -10,27 +10,28 @@
 #include "error.h"
 #include <stdio.h>
 
-
 /**
  * @brief type add_flag_option allows to choose the functions between alu_add16_low and alu_add16_high
  */
-typedef enum {
+typedef enum
+{
     LOW,
     HIGH
 } add_flag_option;
 
-
 /**
  * @brief type define the boolean values in a simple way
  */
-typedef enum {
+typedef enum
+{
     TRUE = 1,
     FALSE = 0
 } boolean;
 
-
-flag_bit_t get_flag(flags_t flags, flag_bit_t flag) {
-    switch(flag) {
+flag_bit_t get_flag(flags_t flags, flag_bit_t flag)
+{
+    switch (flag)
+    {
     case FLAG_Z:
         return FLAG_Z & flags;
     case FLAG_N:
@@ -45,28 +46,33 @@ flag_bit_t get_flag(flags_t flags, flag_bit_t flag) {
     }
 }
 
-void set_flag(flags_t* flags, flag_bit_t flag) {
-	if (flags != NULL) {
-        switch(flag) {
-            case FLAG_Z:
-                *flags = *flags | FLAG_Z;
-                break;
-            case FLAG_N:
-                *flags = *flags | FLAG_N;
-                break;
-            case FLAG_H:
-                *flags = *flags | FLAG_H;
-                break;
-            case FLAG_C:
-                *flags = *flags |FLAG_C;
-                break;
-            default:
-                fprintf(stderr, "Error - Bad parameter not a flag_bit_t %d", flag);
-                break;
+void set_flag(flags_t *flags, flag_bit_t flag)
+{
+    if (flags != NULL)
+    {
+        switch (flag)
+        {
+        case FLAG_Z:
+            *flags = *flags | FLAG_Z;
+            break;
+        case FLAG_N:
+            *flags = *flags | FLAG_N;
+            break;
+        case FLAG_H:
+            *flags = *flags | FLAG_H;
+            break;
+        case FLAG_C:
+            *flags = *flags | FLAG_C;
+            break;
+        default:
+            fprintf(stderr, "Error - Bad parameter not a flag_bit_t %d", flag);
+            break;
         }
-	} else {
+    }
+    else
+    {
         fprintf(stderr, "Error - NULLpointer the given flags_t pointer is NULL");
-	}
+    }
 }
 /**
  * @brief set the flags of result with respect to a couple of parameters
@@ -76,25 +82,32 @@ void set_flag(flags_t* flags, flag_bit_t flag) {
  * @param carry carry bit
  * @param substraction indicates whether it is a subtraction or not
  */
-void handle_flag_setting(alu_output_t* result, bit_t half_carry, bit_t carry, boolean subtraction) {
-    if(half_carry != 0) set_H(&result->flags);
-    if(carry != 0) set_C(&result->flags);
-    if(result->value == 0) set_Z(&result->flags);
-    if(subtraction) set_N(&result->flags);
+void handle_flag_setting(alu_output_t *result, bit_t half_carry, bit_t carry, boolean subtraction)
+{
+    if (half_carry != 0)
+        set_H(&result->flags);
+    if (carry != 0)
+        set_C(&result->flags);
+    if (result->value == 0)
+        set_Z(&result->flags);
+    if (subtraction)
+        set_N(&result->flags);
 }
 
-int alu_add8(alu_output_t* result, uint8_t x, uint8_t y, bit_t c0) {
+int alu_add8(alu_output_t *result, uint8_t x, uint8_t y, bit_t c0)
+{
     M_REQUIRE_NON_NULL(result);
     uint8_t sum_lsb = (uint8_t)(lsb4(x) + lsb4(y) + c0);
     uint8_t c4 = msb4(sum_lsb);
-    uint8_t sum_msb = (uint8_t) (msb4(x) + msb4(y) + c4);
+    uint8_t sum_msb = (uint8_t)(msb4(x) + msb4(y) + c4);
     uint8_t c8 = msb4(sum_msb);
     result->value = merge4(sum_lsb, sum_msb);
     handle_flag_setting(result, c4, c8, FALSE);
     return ERR_NONE;
 }
 
-int alu_sub8(alu_output_t* result, uint8_t x, uint8_t y, bit_t b0) {
+int alu_sub8(alu_output_t *result, uint8_t x, uint8_t y, bit_t b0)
+{
     M_REQUIRE_NON_NULL(result);
     uint8_t sub_lsb = (uint8_t)(lsb4(x) - lsb4(y) - b0);
     uint8_t b4 = msb4(sub_lsb);
@@ -115,51 +128,59 @@ int alu_sub8(alu_output_t* result, uint8_t x, uint8_t y, bit_t b0) {
  * @param y value to sum
  * @param opt option which lets us choose between low and high
  */
-void alu_add16_option(alu_output_t* result, uint16_t x, uint16_t y, add_flag_option opt) {
+void alu_add16_option(alu_output_t *result, uint16_t x, uint16_t y, add_flag_option opt)
+{
     alu_output_t result_low = {0};
     alu_output_t result_high = {0};
     alu_add8(&result_low, lsb8(x), lsb8(y), 0);
     bit_t c8 = get_C(result_low.flags) != 0 ? 1 : 0;
-    alu_add8(&result_high, msb8(x), msb8(y),c8 );
+    alu_add8(&result_high, msb8(x), msb8(y), c8);
     result->value = merge8((uint8_t)result_low.value, (uint8_t)result_high.value);
-    alu_output_t* choice = NULL;
-    switch (opt) {
-        case LOW:
-            choice = &result_low;
-            break;
-        case HIGH:
-            choice = &result_high;
-            break;
-        default:
-            fprintf(stderr, "Error - Bad parameter not an add_flag_option%d", opt);
-            break;
-        }
+    alu_output_t *choice = NULL;
+    switch (opt)
+    {
+    case LOW:
+        choice = &result_low;
+        break;
+    case HIGH:
+        choice = &result_high;
+        break;
+    default:
+        fprintf(stderr, "Error - Bad parameter not an add_flag_option%d", opt);
+        break;
+    }
 
     uint8_t half_carry = get_H(choice->flags);
     uint8_t carry = get_C(choice->flags);
     handle_flag_setting(result, half_carry, carry, FALSE);
 }
 
-int alu_add16_low(alu_output_t* result, uint16_t x, uint16_t y) {
+int alu_add16_low(alu_output_t *result, uint16_t x, uint16_t y)
+{
     M_REQUIRE_NON_NULL(result);
     alu_add16_option(result, x, y, LOW);
     return ERR_NONE;
 }
 
-int alu_add16_high(alu_output_t* result, uint16_t x, uint16_t y) {
+int alu_add16_high(alu_output_t *result, uint16_t x, uint16_t y)
+{
     M_REQUIRE_NON_NULL(result);
     alu_add16_option(result, x, y, HIGH);
     return ERR_NONE;
 }
 
-int alu_shift(alu_output_t* result, uint8_t x, rot_dir_t dir) {
+int alu_shift(alu_output_t *result, uint8_t x, rot_dir_t dir)
+{
     M_REQUIRE_NON_NULL(result);
-    M_REQUIRE((dir == LEFT) | (dir==RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT",dir);
+    M_REQUIRE((dir == LEFT) | (dir == RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT", dir);
     uint8_t carry = 0;
-    if(dir == LEFT) {
+    if (dir == LEFT)
+    {
         result->value = (uint8_t)(x << 1u);
         carry = bit_get(x, 7);
-    } else if(dir == RIGHT) {
+    }
+    else if (dir == RIGHT)
+    {
         result->value = (uint8_t)(x >> 1u);
         carry = bit_get(x, 0);
     }
@@ -167,9 +188,10 @@ int alu_shift(alu_output_t* result, uint8_t x, rot_dir_t dir) {
     return ERR_NONE;
 }
 
-int alu_shiftR_A(alu_output_t* result, uint8_t x) {
+int alu_shiftR_A(alu_output_t *result, uint8_t x)
+{
     M_REQUIRE_NON_NULL(result);
-    bit_t ejected_bit = bit_get(x,0);
+    bit_t ejected_bit = bit_get(x, 0);
     uint8_t carry = ejected_bit != 0;
     uint16_t mask_sign_bit = x & 0x80u;
     result->value = x >> 1u;
@@ -178,25 +200,29 @@ int alu_shiftR_A(alu_output_t* result, uint8_t x) {
     return ERR_NONE;
 }
 
-int alu_rotate(alu_output_t* result, uint8_t x, rot_dir_t dir) {
+int alu_rotate(alu_output_t *result, uint8_t x, rot_dir_t dir)
+{
     M_REQUIRE_NON_NULL(result);
-    M_REQUIRE((dir==LEFT) | (dir==RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT",dir);
+    M_REQUIRE((dir == LEFT) | (dir == RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT", dir);
     bit_t msb = x & 0x80u;
     bit_t lsb = x & 0x01u;
     uint8_t carry = 0;
-    if((dir == LEFT && msb)||(dir == RIGHT && lsb)) carry = 1;
+    if ((dir == LEFT && msb) || (dir == RIGHT && lsb))
+        carry = 1;
     bit_rotate(&x, dir, 1);
     result->value = x;
     handle_flag_setting(result, 0, carry, FALSE);
     return ERR_NONE;
 }
 
-int alu_carry_rotate(alu_output_t* result, uint8_t x, rot_dir_t dir, flags_t flags) {
+int alu_carry_rotate(alu_output_t *result, uint8_t x, rot_dir_t dir, flags_t flags)
+{
     M_REQUIRE_NON_NULL(result);
-    M_REQUIRE((dir == LEFT) | (dir==RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT",dir);
+    M_REQUIRE((dir == LEFT) | (dir == RIGHT), ERR_BAD_PARAMETER, "input value dir (%u) is not LEFT or RIGHT", dir);
     bit_t newCarry = (dir == RIGHT) ? (x & 0x01u) : (x & 0x80u);
-    x = (dir == RIGHT) ? (uint8_t)(x>>1u) : (uint8_t)(x<<1u);
-    if(get_C(flags)!= 0) x = (dir == RIGHT) ? (x | 0x80u) : (x | 0x01u);
+    x = (dir == RIGHT) ? (uint8_t)(x >> 1u) : (uint8_t)(x << 1u);
+    if (get_C(flags) != 0)
+        x = (dir == RIGHT) ? (x | 0x80u) : (x | 0x01u);
     result->value = x;
     handle_flag_setting(result, 0, newCarry, FALSE);
     return ERR_NONE;
--- done/gameboy.c
+++ done/gameboy.c
@@ -5,40 +5,45 @@
  * @author C la vie
  * @date 2020
  */
- 
+
 #include "gameboy.h"
 #include "error.h"
 #include "bootrom.h"
 
-#define DRAW_IMAGE_CYCLES ((uint64_t) 17556)
+#define DRAW_IMAGE_CYCLES ((uint64_t)17556)
 
 #ifdef BLARGG
-static int blargg_bus_listener(gameboy_t* gameboy, addr_t addr){
+static int blargg_bus_listener(gameboy_t *gameboy, addr_t addr)
+{
     M_REQUIRE_NON_NULL(gameboy);
 
-	if(addr == BLARGG_REG ) printf("%c", cpu_read_at_idx(gameboy->cpu, addr));
-	
-	return ERR_NONE;
+    if (addr == BLARGG_REG)
+        printf("%c", cpu_read_at_idx(gameboy->cpu, addr));
+
+    return ERR_NONE;
 }
 #endif
 
-#define INIT_COMPONENT(X,i)({\
-    component_t c;\
-    component_create(&c, MEM_SIZE(X));\
-    gameboy->components[i] = c;\
-    bus_plug(gameboy->bus, &c, X ## _START, X ## _END);\
-})
+#define INIT_COMPONENT(X, i)                            \
+    {                                                   \
+        component_t c;                                  \
+        component_create(&c, MEM_SIZE(X));              \
+        gameboy->components[i] = c;                     \
+        bus_plug(gameboy->bus, &c, X##_START, X##_END); \
+    }                                                   \
+    while (0)
 
-int gameboy_create(gameboy_t* gameboy, const char* filename){
+int gameboy_create(gameboy_t *gameboy, const char *filename)
+{
     M_REQUIRE_NON_NULL(gameboy);
     //Initialise les pointeurs du bus à NULL
     memset(gameboy->bus, 0, BUS_SIZE);
-        
+
     gameboy->boot = 1u;
     bootrom_init(&gameboy->bootrom);
     bootrom_plug(&gameboy->bootrom, gameboy->bus);
 
-    cartridge_init(&gameboy->cartridge,filename);
+    cartridge_init(&gameboy->cartridge, filename);
     cartridge_plug(&gameboy->cartridge, gameboy->bus);
 
     int i = 0;
@@ -49,7 +54,7 @@ int gameboy_create(gameboy_t* gameboy, const char* filename){
     INIT_COMPONENT(VIDEO_RAM, i++);
     INIT_COMPONENT(GRAPH_RAM, i++);
     INIT_COMPONENT(USELESS, i++);
-    
+
     component_t echo_ram;
     /* On n'ajoute pas echo_ram à la liste components car il partage
      * la même mémoire que work_ram cela permet d'eviter de free deux fois
@@ -58,7 +63,7 @@ int gameboy_create(gameboy_t* gameboy, const char* filename){
     bus_plug(gameboy->bus, &echo_ram, ECHO_RAM_START, ECHO_RAM_END);
 
     timer_init(&gameboy->timer, &gameboy->cpu);
-    //c-mem is null 
+    //c-mem is null
     cpu_plug(&gameboy->cpu, &gameboy->bus);
 
     gameboy->cycles = 0;
@@ -66,27 +71,30 @@ int gameboy_create(gameboy_t* gameboy, const char* filename){
     return ERR_NONE;
 }
 
-void gameboy_free(gameboy_t* gameboy){
-    if (gameboy != NULL){
-        // Unplug une copie de echo_ram 
+void gameboy_free(gameboy_t *gameboy)
+{
+    if (gameboy != NULL)
+    {
+        // Unplug une copie de echo_ram
         component_t echo_clone = {NULL, ECHO_RAM_START, ECHO_RAM_END};
         bus_unplug(gameboy->bus, &echo_clone);
 
-        for (size_t i = 0; i < GB_NB_COMPONENTS; ++i) {
+        for (size_t i = 0; i < GB_NB_COMPONENTS; ++i)
+        {
             bus_unplug(gameboy->bus, &gameboy->components[i]);
             component_free(&gameboy->components[i]);
         }
-        
+
         bus_unplug(gameboy->bus, &gameboy->bootrom);
         component_free(&gameboy->bootrom);
-        
+
         bus_unplug(gameboy->bus, &gameboy->cartridge.c);
         cartridge_free(&gameboy->cartridge);
-
     }
 }
 
-int gameboy_run_until(gameboy_t* gameboy, uint64_t cycle){
+int gameboy_run_until(gameboy_t *gameboy, uint64_t cycle)
+{
     while (gameboy->cycles < cycle)
     {
         // TODO une fois que les autres composant auront XX_cycle implémenté
@@ -95,12 +103,13 @@ int gameboy_run_until(gameboy_t* gameboy, uint64_t cycle){
         //TODO il faut check le retour des fonctions
         timer_cycle(&gameboy->timer);
         cpu_cycle(&gameboy->cpu);
-        bootrom_bus_listener(gameboy, gameboy->cpu.write_listener );
+        bootrom_bus_listener(gameboy, gameboy->cpu.write_listener);
         timer_bus_listener(&gameboy->timer, gameboy->cpu.write_listener);
-        if(((gameboy->cycles) % DRAW_IMAGE_CYCLES) == 0) cpu_request_interrupt(&gameboy->cpu, VBLANK);
-        #ifdef BLARGG
+        if (((gameboy->cycles) % DRAW_IMAGE_CYCLES) == 0)
+            cpu_request_interrupt(&gameboy->cpu, VBLANK);
+#ifdef BLARGG
         M_EXIT_IF_ERR(blargg_bus_listener(gameboy, gameboy->cpu.write_listener));
-        #endif
+#endif
         //le nombre de cycles déjà simulés est mis à jour non?
         gameboy->cycles++;
     }
