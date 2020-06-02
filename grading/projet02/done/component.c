23e3aad5258b32fab6e9ae8ea0521874059bed0f
Jean-Cédric Chappelier <jean-cedric.chappelier@epfl.ch>
2020-06-02 15:26:43 +0200

    correction 2e rendu du projet

---
+++ grading/projet02/done/component.c
@@ -0,0 +1,55 @@
+/**
+ * @file component.c
+ * @brief Game Boy Component simulation 
+ *
+ * @date 2020
+ */
+
+#include "component.h"
+#include "error.h"
+#include "memory.h"
+
+int component_create(component_t *c, size_t mem_size)
+{
+    M_REQUIRE_NON_NULL(c);
+
+    c->start = 0;
+    c->end = 0;
+    if (mem_size == 0)
+    {
+        c->mem = NULL;
+        return ERR_NONE;
+    }
+    c->mem = malloc(sizeof(memory_t));
+    if (c->mem == NULL)
+    {
+        return ERR_MEM;
+    }
+
+    return mem_create(c->mem, mem_size);
+}
+
+void component_free(component_t *c)
+{
+    if (c != NULL)
+    {
+        mem_free(c->mem);
+        free(c->mem);
+        c->mem = NULL;
+        c->start = 0;
+        c->end = 0;
+    }
+}
+
+int component_shared(component_t *c, component_t *c_old)
+{
+    M_REQUIRE_NON_NULL(c);
+    M_REQUIRE_NON_NULL(c_old);
+    M_REQUIRE_NON_NULL_CUSTOM_ERR(c_old->mem, ERR_MEM);
+
+    c->start = 0;
+    c->end = 0;
+    c->mem = c_old->mem;
+
+    return ERR_NONE;
+}
